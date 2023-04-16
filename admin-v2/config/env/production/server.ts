c8ec262044bba08bc64514eef388383e11d2a41e
PhilipSh <fil3120025@gmail.com>
2023-04-17 01:42:16 +0300

    chore: production env config

---
+++ admin-v2/config/env/production/server.ts
@@ -0,0 +1,11 @@
+export default ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://world-nsp.com/strapi",
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+});
