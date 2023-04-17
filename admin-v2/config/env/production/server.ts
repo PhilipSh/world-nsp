f2ae1f28b9b4666c82cd80947863655692ca6eb7
PhilipSh <fil3120025@gmail.com>
2023-04-17 14:02:49 +0300

    chore: admin server config

--- admin-v2/config/env/production/server.ts
+++ admin-v2/config/env/production/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://world-nsp.com/strapi",
+  url: "https://world-nsp.com/strapi",
   app: {
     keys: env.array("APP_KEYS"),
   },
