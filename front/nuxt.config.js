a4174516c8cb1a0716fb1c5b0d4e28d937658b0e
PhilipSh <fil3120025@gmail.com>
2023-07-06 23:30:26 +0300

    feat(front): add sitemap module

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ["@nuxtjs/axios"],
+  modules: ["@nuxtjs/axios", "@nuxtjs/sitemap"],
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     postcss: {
